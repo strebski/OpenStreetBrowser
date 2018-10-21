c3fb4e904737827d1e40fcafda925966b7a60b7f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-10-21 19:15:37 +0200

    CategoryOverpass/Filter: add parameter to url resp. read; improve form handling

--- src/categories.js
+++ src/categories.js
@@ -1,3 +1,5 @@
+var queryString = require('query-string')
+
 var OpenStreetBrowserLoader = require('./OpenStreetBrowserLoader')
 
 register_hook('state-apply', function (state) {
@@ -7,18 +9,31 @@ register_hook('state-apply', function (state) {
 
   var list = state.categories.split(',')
   list.forEach(function (id) {
+    let param
+
+    let m = id.match(/^([0-9A-Z_-]+)(\[(.*)\])/i)
+    if (m) {
+      id = m[1]
+      param = queryString.parse(m[3])
+    }
+
     OpenStreetBrowserLoader.getCategory(id, function (err, category) {
+      console.log('here', id, param)
       if (err) {
         console.log("Can't load category " + id + ': ', err)
         return
       }
 
       if (category) {
-        category.open()
+        if (param) {
+          category.setParam(param)
+        }
 
         if (!category.parentDom) {
           category.setParentDom(document.getElementById('contentList'))
         }
+        category.open()
+
       }
     })
   })
